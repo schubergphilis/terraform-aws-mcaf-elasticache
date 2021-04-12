236b4cefa24bb48f3a7ab40c05667848d3ea0d34
lasseb <lbenninga@schubergphilis.com>
2021-04-12 14:11:25 +0200

    Output replication group endpoints

--- output.tf
+++ output.tf
@@ -1,9 +1,11 @@
+output "primary_endpoints" {
+  value = var.num_cache_nodes == 1 ? aws_elasticache_cluster.default[0].cache_nodes.*.address : aws_elasticache_replication_group.default[0].primary_endpoint_address
+}
+
+output "reader_endpoints" {
+  value = var.num_cache_nodes > 1 ? aws_elasticache_replication_group.default[0].reader_endpoint_address : null
+}
+
 output "security_group_id" {
   value = aws_security_group.elasticache.id
-}
-
-output "endpoints" {
-  value = aws_elasticache_cluster.default.cache_nodes.*.address
-}
-
 }
